7442387eebe8835ddc8ee3b6dea8e8a08d082c20
hariven <harivent29@gmail.com>
2025-01-11 17:38:51 +0800

    feat: Add Habit entity with id, name, and createdAt properties

---
+++ backend/src/modules/Domain/Habit/index.ts
@@ -0,0 +1,15 @@
+// import 'reflect-metadata';
+// import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+// @Entity()
+// export class Habit {
+//     @PrimaryGeneratedColumn()
+//     id: number
+
+//     @Field()
+//     @Column()
+//     name: string
+
+//     @Column()
+//     createdAt: Date
+// }
