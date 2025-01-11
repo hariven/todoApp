92e668faae0e84bc15725f2cb3df1768de3f69cb
hariven <harivent29@gmail.com>
2025-01-11 17:46:14 +0800

    updated

--- backend/src/modules/Domain/Habit/index.ts
+++ backend/src/modules/Domain/Habit/index.ts
@@ -1,7 +1,7 @@
 // import 'reflect-metadata';
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+// import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-@Entity()
+// @Entity()
 // export class Habit {
 //     @PrimaryGeneratedColumn()
 //     id: number
